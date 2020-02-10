52ee5df8756c1b5c0156b2b59b39e05f0d85f7fd
uzair2299 <uzairanwar2299@gmail.com>
2020-02-11 00:48:40 +0500

    setting theme2

--- AutoSolution/Global.asax.cs
+++ AutoSolution/Global.asax.cs
@@ -15,7 +15,7 @@ namespace AutoSolution
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            
         }
     }
 }
