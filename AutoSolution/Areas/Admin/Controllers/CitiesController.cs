06fe806f976dba3373f409d96593ac7455b0dd16
uzair2299 <uzairanwar2299@gmail.com>
2020-02-29 13:16:00 +0500

    addedd

---
+++ AutoSolution/Areas/Admin/Controllers/CitiesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutoSolution.Areas.Admin.Controllers
+{
+    public class CitiesController : Controller
+    {
+        // GET: Admin/Cities
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
